f5b8d7aa3419dda3fad0a9d600417ccc7f2c2cf0
YongWoon_Yun <yuyun0124@gmail.com>
2023-01-29 21:35:44 +0900

    fix: NotFoundException 부모 변경

--- src/main/java/com/bcsdlab/biseo/exception/NotFoundException.java
+++ src/main/java/com/bcsdlab/biseo/exception/NotFoundException.java
@@ -2,7 +2,7 @@ package com.bcsdlab.biseo.exception;
 
 import com.bcsdlab.biseo.enums.ErrorMessage;
 
-public class NotFoundException extends BaseException{
+public class NotFoundException extends NonCriticalException {
 
     public NotFoundException(ErrorMessage errorMessage) {
         super(errorMessage);
